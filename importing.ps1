4651b1d89ebe637b43676ce50e93d14e9ddb48a0
Zenn1th <33702673+Zenn1th@users.noreply.github.com>
2018-01-08 20:39:50 +0100

    Add files via upload

---
+++ importing.ps1
@@ -0,0 +1,2 @@
+﻿Import-Module ActiveDirectory
+Import-Csv users.csv | ForEach-Object {New-ADUser -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -Force) -SamAccountName $_.SamAccountName -GivenName $_.FirstName -Surname $_.LastName -Name $_.Name -UserPrincipalName $_.UPN -Enabled $true}
